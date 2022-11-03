a1e1a7465a2c6968e8a12183cb33d3af1749f283
unknown <slashley127@gmail.com>
2022-11-02 19:26:16 -0500

    final changes completed, all tests pass

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -30,7 +30,7 @@ public class SearchController {
     @PostMapping("results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
         ArrayList<Job> jobs;
-        if (searchType.equals("all") && searchTerm.isBlank()){
+        if (searchType.equals("all") || searchTerm.isBlank()){
             jobs = JobData.findAll();
             model.addAttribute("title", "All Jobs");
         } else {
